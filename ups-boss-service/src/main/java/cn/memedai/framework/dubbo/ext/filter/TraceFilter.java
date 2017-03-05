0d5ff44ed7543163b82da0dae6324389069bfe7d
xiong <2411867257@qq.com>
2017-03-05 21:12:56 +0800

    修改TraceFilter

--- ups-boss-service/src/main/java/cn/memedai/framework/dubbo/ext/filter/TraceFilter.java
+++ ups-boss-service/src/main/java/cn/memedai/framework/dubbo/ext/filter/TraceFilter.java
@@ -1,6 +1,11 @@
 package cn.memedai.framework.dubbo.ext.filter;
 
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.MDC;
+
 import lombok.extern.slf4j.Slf4j;
+
+import cn.memedai.ups.boss.utils.StrUtil;
 
 import com.alibaba.dubbo.common.extension.Activate;
 import com.alibaba.dubbo.common.Constants;
@@ -8,6 +13,7 @@ import com.alibaba.dubbo.rpc.Filter;
 import com.alibaba.dubbo.rpc.Invocation;
 import com.alibaba.dubbo.rpc.Invoker;
 import com.alibaba.dubbo.rpc.Result;
+import com.alibaba.dubbo.rpc.RpcContext;
 import com.alibaba.dubbo.rpc.RpcException;
 @Slf4j
 @Activate(group = {Constants.PROVIDER, Constants.CONSUMER})
@@ -17,6 +23,20 @@ public class TraceFilter implements Filter{
 	public Result invoke(Invoker<?> invoker, Invocation invocation)
 			throws RpcException {
 		log.info("\n___________开始调用dubbo__________");
+		
+		String traceId = (String) invocation.getAttachments().get("THREAD_ID");//, MDC.get("THREAD_ID")
+		if(StringUtils.isNotBlank(traceId)){
+			MDC.put("THREAD_ID",traceId);
+		}else{
+			traceId = MDC.get("THREAD_ID");
+			if(StringUtils.isNotBlank(traceId)){
+				//RpcContext.getContext().set("THREAD_ID", traceId);
+				invocation.getAttachments().put("THREAD_ID", traceId);
+			}else{
+				MDC.put("THREAD_ID",StrUtil.get32UUID());
+				invocation.getAttachments().put("THREAD_ID", StrUtil.get32UUID());
+			}
+		}
 		Result result = invoker.invoke(invocation);
 		return result;
 	}
